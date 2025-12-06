3a3cbf1890cc6d28ad655a4c65c3d26ce74e644d
AyeshaAbbasi <ayesha13abbasi@gmail.com>
2025-12-07 04:03:21 +0500

    Update test_todo_app.py

--- selenium-tests/test_todo_app.py
+++ selenium-tests/test_todo_app.py
@@ -65,7 +65,7 @@ def setup_test_user():
 def driver():
     
         chrome_options = Options()
-         
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
